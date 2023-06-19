0e860190ab40f7dd731d38924169d2a335e75384
hayas1 <h4ystack@gmail.com>
2023-06-19 11:08:25 +0000

    latency log

--- api/src/logging/middleware.rs
+++ api/src/logging/middleware.rs
@@ -1,12 +1,17 @@
 use axum::response::IntoResponse;
+use tokio::time::Instant;
 
 pub async fn request_log<B>(
     req: hyper::Request<B>,
     next: axum::middleware::Next<B>,
 ) -> impl IntoResponse {
     let (method, uri) = (req.method().clone(), req.uri().clone());
+
+    let start = Instant::now();
     let res = next.run(req).await;
+    let latency = start.elapsed();
+
     // TODO better log
-    tracing::info!("{} {} {}", method, uri, res.status());
+    tracing::info!("{} {} {} {:?}", method, uri, res.status(), latency);
     res
 }
