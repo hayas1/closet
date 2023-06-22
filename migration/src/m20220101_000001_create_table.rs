fde6afb20c0a95a2e83f29b33105eaefd8d1726f
hayas1 <h4ystack@gmail.com>
2023-06-22 13:37:29 +0000

    migration

---
+++ migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,22 @@
+use entity::user;
+use sea_orm_migration::{prelude::*, sea_orm::Schema};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_database_backend();
+        let schema = Schema::new(db);
+        manager
+            .create_table(schema.create_table_from_entity(user::Entity))
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(user::Entity).to_owned())
+            .await
+    }
+}
