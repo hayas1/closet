e2e28011c6d4c74acb3fb8a8a562ed1daa11246f
hayas1 <h4ystack@gmail.com>
2023-06-23 15:41:58 +0000

    seeding health check data to db

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -1,5 +1,7 @@
-use entity::user;
-use sea_orm_migration::{prelude::*, sea_orm::Schema};
+use sea_orm_migration::{
+    prelude::*,
+    sea_orm::{ActiveValue, EntityTrait, QueryTrait, Schema},
+};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -7,16 +9,27 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        let db = manager.get_database_backend();
-        let schema = Schema::new(db);
+        let schema = Schema::new(manager.get_database_backend());
         manager
-            .create_table(schema.create_table_from_entity(user::Entity))
-            .await
+            .create_table(schema.create_table_from_entity(entity::user::Entity))
+            .await?;
+        manager
+            .create_table(schema.create_table_from_entity(entity::health::Entity))
+            .await?;
+
+        let insert_ok = entity::health::Entity::insert(entity::health::ActiveModel {
+            status: ActiveValue::Set("ok".into()),
+        });
+        manager.exec_stmt(insert_ok.into_query()).await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .drop_table(Table::drop().table(user::Entity).to_owned())
-            .await
+        let (drop_user_table, drop_health_table) = (
+            manager.drop_table(Table::drop().table(entity::user::Entity).to_owned()),
+            manager.drop_table(Table::drop().table(entity::health::Entity).to_owned()),
+        );
+        let (rut, rht) = tokio::join!(drop_user_table, drop_health_table);
+        (rut?, rht?);
+        Ok(())
     }
 }
