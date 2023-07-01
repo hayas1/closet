c8ed5772d0cbb075038cc7d5a1b9758a4d4bdba3
hayas1 <h4ystack@gmail.com>
2023-07-01 06:50:21 +0000

    test login logout scenario

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -54,6 +54,7 @@ pub async fn standalone() -> Result<axum::Router, sea_orm::DbErr> {
         database_url: Some("sqlite::memory:".into()),
         secret_key: Some(Alphanumeric.sample_string(&mut rand::thread_rng(), 1024)),
         migrate: Some(true),
+        timeout: Some("1d".into()),
         ..Default::default()
     });
     with_auth(router(configuration.base_url()), configuration).await
@@ -102,5 +103,168 @@ mod tests {
         assert_eq!(&bytes[..], br#"{"result":{"status":"ok"}}"#);
         let health: ApiResponse<RichHealth> = serde_json::from_slice(&bytes).unwrap();
         assert_eq!(health, ApiResponse::new(RichHealth { status: Status::Ok }));
+    }
+
+    #[tokio::test]
+    #[cfg(feature = "sqlite")]
+    async fn test_login_logout_scenario() {
+        use entity::class::password::Password;
+
+        use crate::{
+            handler::auth::{UserCreate, UserLogin},
+            middleware::authorization::AuthUser,
+            response::error::ApiError,
+        };
+
+        let api = standalone().await.unwrap();
+
+        let create = UserCreate {
+            display_name: "hogehoge".into(),
+            email: "hoge@fuga.piyo".into(),
+            username: "fugafuga".into(),
+            password: "piyopiyo".into(),
+        };
+        let created_response = api
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .method(hyper::Method::POST)
+                    .uri("/auth/create")
+                    .header(hyper::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(serde_json::json!(create).to_string()))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        let created_user: ApiResponse<AuthUser> =
+            serde_json::from_slice(&to_bytes(created_response.into_body()).await.unwrap()).unwrap();
+        assert!(matches!(created_user.result().token, None));
+        assert_eq!(created_user.result().user.display_name, "hogehoge");
+        assert_eq!(created_user.result().user.username.to_string(), "fugafuga");
+        assert_eq!(created_user.result().user.email.to_string(), "hoge@fuga.piyo");
+        assert_eq!(created_user.result().user.password, Password::Unauthenticated);
+
+        let invalid = UserLogin { username: "fugafuga".into(), password: "pw".into() };
+        let forbidden_login_response = api
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .method(hyper::Method::POST)
+                    .uri("/auth/login")
+                    .header(hyper::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(serde_json::json!(invalid).to_string()))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        let forbidden_login: serde_json::Value =
+            serde_json::from_slice(&to_bytes(forbidden_login_response.into_body()).await.unwrap())
+                .unwrap();
+        assert!(matches!(
+            // TODO deserialize error
+            serde_json::from_value(forbidden_login["error"]["serde"].clone().into()).unwrap(),
+            ApiError::LoginFailError
+        ));
+
+        let login = UserLogin { username: "fugafuga".into(), password: "piyopiyo".into() };
+        let login_response = api
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .method(hyper::Method::POST)
+                    .uri("/auth/login")
+                    .header(hyper::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(serde_json::json!(login).to_string()))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        let auth_user: ApiResponse<AuthUser> =
+            serde_json::from_slice(&to_bytes(login_response.into_body()).await.unwrap()).unwrap();
+        assert!(matches!(auth_user.result().token, Some(_)));
+        assert_eq!(auth_user.result().user.display_name, "hogehoge");
+        assert_eq!(auth_user.result().user.username.to_string(), "fugafuga");
+        assert_eq!(auth_user.result().user.email.to_string(), "hoge@fuga.piyo");
+        assert_eq!(auth_user.result().user.password, Password::Unauthenticated);
+        assert_ne!(created_user.result().user, auth_user.result().user);
+        assert_eq!(
+            entity::model::user::Model {
+                updated_at: Default::default(),
+                ..created_user.result().user.clone()
+            },
+            entity::model::user::Model {
+                last_login: None,
+                updated_at: Default::default(),
+                ..auth_user.result().user.clone()
+            }
+        );
+
+        let access_token = auth_user.result().token.as_ref().unwrap();
+        let whoami_response = api
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .uri("/auth/whoami")
+                    .header(hyper::header::AUTHORIZATION, format!("Bearer {}", access_token))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        let whoami_user: ApiResponse<AuthUser> =
+            serde_json::from_slice(&to_bytes(whoami_response.into_body()).await.unwrap()).unwrap();
+        assert!(matches!(whoami_user.result().token, Some(_)));
+        assert_eq!(whoami_user.result().user.display_name, "hogehoge");
+        assert_eq!(whoami_user.result().user.username.to_string(), "fugafuga");
+        assert_eq!(whoami_user.result().user.email.to_string(), "hoge@fuga.piyo");
+        assert_eq!(whoami_user.result().user.password, Password::Unauthenticated);
+        assert_eq!(auth_user, whoami_user);
+
+        let logout_response = api
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .uri("/auth/logout")
+                    .method(hyper::Method::POST)
+                    .header(hyper::header::AUTHORIZATION, format!("Bearer {}", access_token))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        let logout_user: ApiResponse<AuthUser> =
+            serde_json::from_slice(&to_bytes(logout_response.into_body()).await.unwrap()).unwrap();
+        assert!(matches!(logout_user.result().token, None));
+        assert_eq!(logout_user.result().user.display_name, "hogehoge");
+        assert_eq!(logout_user.result().user.username.to_string(), "fugafuga");
+        assert_eq!(logout_user.result().user.email.to_string(), "hoge@fuga.piyo");
+        assert_eq!(logout_user.result().user.password, Password::Unauthenticated);
+        assert_ne!(whoami_user, logout_user);
+        assert_eq!(
+            entity::model::user::Model {
+                updated_at: Default::default(),
+                ..whoami_user.result().user.clone()
+            },
+            entity::model::user::Model {
+                last_logout: None,
+                updated_at: Default::default(),
+                ..logout_user.result().user.clone()
+            }
+        );
+
+        let no_auth_response = api
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .uri("/auth/whoami")
+                    .header(hyper::header::AUTHORIZATION, format!("Bearer {}", access_token))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        let no_auth_whoami: ApiResponse<Option<AuthUser>> =
+            serde_json::from_slice(&to_bytes(no_auth_response.into_body()).await.unwrap()).unwrap();
+        assert!(matches!(no_auth_whoami.result(), None));
     }
 }
